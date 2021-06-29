d567d3abea9bdf4ec71315fdf97f99bf865d8707
yuta8995 <spcrnjmk9705@gmail.com>
2021-06-30 04:47:54 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,10 @@ class ItemsController < ApplicationController
   def index
   end
 
+  # private
+
+  # def message_params
+    # params.require(:item).permit(:item_name, :item_description, :category_id, :item_condition_id, :shipping_paid_by_id, :item_location_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
+  # end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
   # has_one    :buyer
+  has_one_attached :image
 
 end
