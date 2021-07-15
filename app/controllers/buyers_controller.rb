9368691939023dd370da1172ce07975a03911e9e
yuta8995 <spcrnjmk9705@gmail.com>
2021-07-16 02:19:32 +0900

    フォームの情報を保存できるようにする

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,10 +1,36 @@
 class BuyersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: :index
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @buyer_address = BuyerAddress.new
   end
 
   def create
+    @buyer_address = BuyerAddress.new(buyer_params)
+    if @buyer_address.valid?
+      @buyer_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def buyer_params
+    params.require(:buyer_address).permit(:postal_code, :item_location_id, :municipality, :house_number, :building_name, :tel).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if current_user == @item.user
+      redirect_to root_path
+    end
   end
 
 end
